60276d30d073e25a9ee8bf96c705fa44ae31b224
Tran Van Minh <s3585377@rmit.edu.vn>
2017-07-15 20:29:17 +0700

    Only keep parts of Q1 that Tuan would need to simplify merging latter Update crackTransposition() so that it can be apply for anything

--- src/Q2.java
+++ src/Q2.java
@@ -1,7 +1,5 @@
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.TreeMap;
 
 
 public class Q2 {
@@ -13,40 +11,17 @@ public class Q2 {
     *           index = OLD_STRING.LENGTH/I*j+k
     *
     * */
-
-    static void answer() throws IOException {
-        ArrayList<Character> old_string = Q1.readFile("sourceFile/msg2.enc");
-        ArrayList<Integer> commonDivisors = findCommonDivisors(old_string.size());
-//        TreeMap<String, Integer> mostCommonWords = Q1.process10000file();
-//        DecodedString[] resultsAsArray = new DecodedString[commonDivisors.size()];
-        for (int i : commonDivisors) {
-            String result = "";
-            for (int k = 0; k < old_string.size() / i; k++) {
-                for (int j = 0; j < i; j++) {
-                    int index = old_string.size() / i * j + k;
-                    result += old_string.get(index);
-                }
-            }
-            //print result from here
-            System.out.println("Key    " + i);
-            System.out.println("Decoded string:\n\n" + result + "\n");
+    public static void main(String[] args) {
+        try {
+            crackTransposition("sourceFile/msg2.enc", -1);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        // TODO implement scoring method
-//            resultsAsArray[i] = new DecodedString(result, i, Q1.scoring(result, mostCommonWords));
-//        }
-//        Arrays.sort(resultsAsArray, resultsAsArray[0]);
-//        System.out.println("TOP 3 RESULTS OF QUESTION 2\n");
-//        for (int i = resultsAsArray.length - 1; i > resultsAsArray.length - 4; i--) {
-//            System.out.println("Key    " + resultsAsArray[i].getKey());
-//            System.out.println("Score  " + resultsAsArray[i].getScore());
-//            System.out.println("Decoded string:\n\n" + resultsAsArray[i].getDecodedString() + "\n");
-//        }
-//        System.out.println();
     }
 
     private static ArrayList<Integer> findCommonDivisors(int size) {
         ArrayList<Integer> commonDivisors = new ArrayList<>();
-        for (int i = 1; i < size; i++) {
+        for (int i = 1; i < size / 2 + 1; i++) {
             if (size%i ==0) {
                 commonDivisors.add(i);
             }
@@ -54,11 +29,31 @@ public class Q2 {
         return commonDivisors;
     }
 
-    public static void crackTransposition(String path, int key) throws IllegalArgumentException {
-        if (key == -1) {
-            System.out.println("There is no key provided");
-        } else if (key > -1) {
-            System.out.println("There is at least a key provided");
+    static void crackTransposition(String path, int key) throws IllegalArgumentException, IOException {
+//        TODOx fix this one
+        ArrayList<Character> old_string = Q1.readFile(path);
+        ArrayList<Integer> commonDivisors = findCommonDivisors(old_string.size());
+        if (key != -1 && !commonDivisors.contains(key)) {
+            throw new IllegalArgumentException("Invalid key value!");
         }
+        if (key == -1) {
+            for (int i : commonDivisors) {
+                actualCrack(i, old_string);
+            }
+        } else if (key > -1) {
+            actualCrack(key, old_string);
+        }
+    }
+
+    private static void actualCrack(int key, ArrayList<Character> old_string) {
+        StringBuilder result = new StringBuilder();
+        for (int k = 0; k < old_string.size() / key; k++) {
+            for (int j = 0; j < key; j++) {
+                int index = old_string.size() / key * j + k;
+                result.append(old_string.get(index));
+            }
+        }
+        System.out.println("Key    " + key);
+        System.out.println("Decoded string:\n\n" + result + "\n");
     }
 }
